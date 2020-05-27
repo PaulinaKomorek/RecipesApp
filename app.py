e6836c22dec9e5c48b575c2015447c447ae752b0
PaulinaKomorek <paulina-komorek@wp.pl>
2020-05-27 23:23:25 +0200

    Indentation unification

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
-from database import db
+from database import *
 from queries import *
 
 app = Flask(__name__)
@@ -11,19 +11,20 @@ db.init_app(app)
 
 @app.route("/", methods=["get"])
 def index():
-	return render_template("index.html")
+    return render_template("index.html")
 
 
 @app.route("/search", methods=["get"])
 def search():
-	products_idx = list(map(lambda i: int(i), request.args.get("products").split(",")))
-	queries = get_recipies_by_products_idx(products_idx)
-	return render_template("search.html", queries=queries)
+    products_idx = list(
+        map(lambda i: int(i), request.args.get("products").split(",")))
+    queries = get_recipies_by_products_idx(products_idx)
+    return render_template("search.html", queries=queries)
 
 
 @app.route("/recipe/<idx>", methods=["get"])
 def recipe(idx: int):
-    recipe=Recipe.query.get(idx)
+    recipe = Recipe.query.get(idx)
     if recipe is None:
         return render_template("not_found.html")
     return render_template("recipe.html", recipe=recipe)
